920daa3ca74a9ad04cb0b04d038cdbc37e8b562a
Seun1601 <114428457+Seun1601@users.noreply.github.com>
2022-10-02 18:41:30 -0700

    nav icons work now

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
 function myFunction(x) {
     x.classList.toggle("change");
   }
+//   document.getElementById("demo").onclick = function() {valueChanged()};
+//   function valueChanged()
+//   {
+//       if($('.search').is(":checked"))   
+//           $(".searchbox").show();
+//       else
+//           $(".searchbox").hide();
+//   }
 
+const trigger = document.querySelector('.trigger');
+const searchinput = document.querySelector('.searchinput');
+const links = document.querySelector('.links')
+trigger.addEventListener('click', () => {
+    if(!searchinput.classList.contains('searchinput-open')){
+        searchinput.classList.add('searchinput-open');
+        trigger.innerHTML = "<i class='fas fa-times'></i>";
+        links.classList.add('links-hide');
+    }
+    else{
+        searchinput.classList.remove('searchinput-open');
+        links.classList.remove('links-hide');
+        trigger.innerHTML = "<i class='fa-solid fa-magnifying-glass fa-lg'></i>";
+
+    }
+
+});
